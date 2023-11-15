8ea8b5687a0e96d47ed91c34f1b743dedacb4fef
Luís Henrique Krüger <luishkruger@gmail.com>
2023-11-15 15:20:21 -0300

    Exit Funcion

--- TitleScreen.gd
+++ TitleScreen.gd
@@ -1,11 +1,9 @@
 extends Control
 
-@onready var start: Button = $Start
-# Called when the node enters the scene tree for the first time.
-func _ready():
+
+func _on_exit_pressed():
+	get_tree().quit()
+
+
+func _on_start_pressed():
 	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
