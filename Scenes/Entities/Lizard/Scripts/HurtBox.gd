041dcbcc7cf122e0b5f603fcaf2ce4b510694632
Gustavo Girão <56512641+Mufado@users.noreply.github.com>
2023-11-29 00:26:30 -0300

    Fix suicide lizard bug

--- Scenes/Entities/Lizard/Scripts/HurtBox.gd
+++ Scenes/Entities/Lizard/Scripts/HurtBox.gd
@@ -1,16 +1,1 @@
-class_name HurtBox
-extends Area2D
-
-func _init() -> void:
-	collision_layer = 128
-	collision_mask = 64
-
-func _ready():
-	connect("area_entered", _on_area_entered)
-
-func _on_area_entered(hitbox: HitBox) -> void:
-	if hitbox == null:
-		return
-
-	if owner.has_method("take_damage"):
-		owner.take_damage(hitbox.damage)
+extends HurtBox
